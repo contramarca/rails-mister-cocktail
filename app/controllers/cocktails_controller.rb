1828c4cf52f1c3530ee44d471eedf37065f3d1f9
Jaime Cruz <jaimecruza@me.com>
2019-08-08 17:58:14 +0200

    models controllers and starting views

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,46 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(strong_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+  end
+
+  private
+
+  def strong_params
+    params.require(:cocktail).permit(:name, :img_url)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri-s3'
+require 'json'
+
+puts 'Cleaning database...'
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+ingredient_parameters = []
+
+url_ingredients = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_json_file = open(url_ingredients).read
+result_ingredients = JSON.parse(ingredient_json_file)
+
+result_ingredients['drinks'].each do |element|
+  seed_ingredient = {
+    name: element['strIngredient1']
+  }
+  ingredient_parameters << seed_ingredient
+end
+
+
+ingredients = Ingredient.create(ingredient_parameters)
+puts 'Finished ingredients.'
+puts 'Creating cocktails...'
+
+cocktail_parameters = []
+
+url_cocktails = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+cocktail_json_file = open(url_cocktails).read
+result_cocktails = JSON.parse(cocktail_json_file)
+
+result_cocktails['drinks'].each do |element|
+  seed_cocktail = {
+    name: element['strDrink'],
+    img_url: element['strDrinkThumb']
+  }
+  cocktail_parameters << seed_cocktail
+end
+
+cocktails = Cocktail.create(cocktail_parameters)
+puts 'Finished cocktails.'
+
+puts 'Creating doses...'
+dose_parameters = []
+url_doses = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007'
+
+dose_json_file = open(url_doses).read
+result_doses = JSON.parse(dose_json_file)
+
+result_doses['drinks'].each do |element|
+  150.times do
+    seed_dose = {
+      description: element['strInstructions'],
+      cocktail: cocktails.sample,
+      ingredient: ingredients.sample
+    }
+    dose_parameters << seed_dose
+  end
+end
+
+Dose.create(dose_parameters)
+puts 'Finished doses.'
